84a33f456d7d273777e9036736c8ec76d3064441
Takeshi Shiomi <takeyan403@gmail.com>
2024-08-15 16:30:27 +0900

    モデルスペック追加

--- backend/spec/models/api/v1/gear_spec.rb
+++ backend/spec/models/api/v1/gear_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
-
 RSpec.describe Api::V1::Gear, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:gear_instance) { create(:gear) }
+
+  describe '#ratio' do
+    it 'チェーンリングとコグの比率を計算する' do
+      expect(gear_instance.ratio).to eq(BigDecimal("9.99") / BigDecimal("9.98"))
+    end
+
+    it 'BigDecimalを返す' do
+      expect(gear_instance.ratio).to be_a(BigDecimal)
+    end
+  end
 end
--- backend/spec/models/api/v1/wheel_spec.rb
+++ backend/spec/models/api/v1/wheel_spec.rb
@@ -1,5 +1,24 @@
-require 'rails_helper'
-
 RSpec.describe Api::V1::Wheel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:wheel_instance) { create(:wheel) }
+
+  describe '#diameter' do
+    it 'ホイールの直径を計算する' do
+      expect(wheel_instance.diameter).to eq(BigDecimal("19.99") + BigDecimal("29.99") * 2)
+    end
+
+    it 'BigDecimalを返す' do
+      expect(wheel_instance.diameter).to be_a(BigDecimal)
+    end
+  end
+
+  describe '#circumference' do
+    it 'ホイールの円周を計算する' do
+      expected_circumference = (BigDecimal("19.99") + BigDecimal("29.99") * 2) * Math::PI
+      expect(wheel_instance.circumference).to eq(expected_circumference)
+    end
+
+    it 'BigDecimalを返す' do
+      expect(wheel_instance.circumference).to be_a(BigDecimal)
+    end
+  end
 end
